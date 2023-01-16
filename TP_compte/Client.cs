8134a0d15677fd37018fede9550d13afa5057b2d
cda07saba <jeremy.sabadie@2isa.org>
2023-01-16 10:52:37 +0100

    Correction de l'erreur d'acolades - Supréssion de Amount dans le constructeur Client

--- TP_compte/Client.cs
+++ TP_compte/Client.cs
@@ -1,69 +1,77 @@
-﻿namespace TP_compte
+﻿namespace TP_compte;
+
+internal class Client
 {
-    internal class Client
+    //Attributs de la classe:
+    private string _Nom;
+    private string _Prenom;
+    private string _Tel;
+    private int _CIN;
+    private static int count = 0;
+
+    //constructeur permettant d’initialiser tous les attributs: 
+    public Client(string nom, string prenom, string tel, int CIN)
     {
-        //Attributs de la classe:
-        private string _Nom;
-        private string _Prenom;
-        private string _Tel;
-        private int _CIN;
-        public int _count = 0;
-
-        //constructeur permettant d’initialiser tous les attributs: 
-        public Client(string nom, string prenom, string tel, int CIN)
-        {
-            _Nom = nom;
-            _Prenom = prenom;
-            _Tel = tel;
-            _CIN = CIN;
-            //Compteur du nombre de création de compte par les constructeurs.
-            Count++;
-
-
-        }
-        //Constructeur permettant d’initialiser le CIN, le nom et le prénom.
-        public Client(string nom, string prenom, int CIN)
-        {
-            _Nom = nom;
-            _Prenom = prenom;
-            _CIN = CIN;
-
-        }
-        //Constructeur quirécupère les valeurs des propriétés en console:
-        public Client()
-        {
-            Console.WriteLine($"Compte {Count}");
-            Console.WriteLine("Donner leCIN:");
-            CIN = int.Parse(Console.ReadLine());
-            Console.WriteLine("Donner le nom:");
-            Nom = Console.ReadLine();
-            Console.WriteLine("Donner leCIprénom:");
-            Prenom = Console.ReadLine();
-            Console.WriteLine("Donner le numéro de téléphone:");
-            Tel = Console.ReadLine();
-            Console.WriteLine("Montant à déposer?");
-            public int Amount = int.Parse(Console.ReadLine());
+        Nom = nom;
+        Prenom = prenom;
+        Tel = tel;
+        this.CIN = CIN;
+        //Compteur du nombre de création de compte par les constructeurs.
         Count++;
-        }
+
+
+    }
+    //Constructeur permettant d’initialiser le CIN, le nom et le prénom.
+    public Client(string nom, string prenom, int CIN)
+    {
+        Nom = nom;
+        Prenom = prenom;
+        this.CIN = CIN;
+        Compte BankCompt;
+        int Count;
+
+
+    }
+    //Constructeur quirécupère les valeurs des propriétés en console:
+    public Client()
+    {
+        Console.WriteLine($"Compte {Count}");
+        Console.WriteLine("Donner leCIN:");
+        CIN = int.Parse(Console.ReadLine());
+        Console.WriteLine("Donner le nom:");
+        Nom = Console.ReadLine();
+        Console.WriteLine("Donner leCIprénom:");
+        Prenom = Console.ReadLine();
+        Console.WriteLine("Donner le numéro de téléphone:");
+        Tel = Console.ReadLine();
+        Console.WriteLine("Montant à déposer?");
+        Amount = int.Parse(Console.ReadLine());
+        Count++;
+    }
 
     //Encapsulation des attributs en utilisant les propriétés
-    public string Nom { public get => _Nom; public set => _Nom = value; }
-    public string Prenom { public get => _Prenom; public set => _Prenom = value; }
-    public string Tel { public get => _Tel; public set => _Tel = value; }
+    public string Nom { get => _Nom; set => _Nom = value; }
+    public string Prenom { get => _Prenom; set => _Prenom = value; }
+    public string Tel { get => _Tel; set => _Tel = value; }
     public int CIN { get => _CIN; set => _CIN = value; }
-    public int Count { public get => _count; private set => _count = value; }
+    public static int Count { get => count; set => count = value; }
+
+    public int Amount { get; set; }
 
     //Méthode Afficher qui affiche les information du client:
     public void Afficher(Client client)
     {
         Console.WriteLine("Détail du compte:");
-        Console.WriteLine($"nom:{client.Nom}");
-        Console.WriteLine($"nom:{client.Nom}");
+        Console.WriteLine($"prénom:{client.Prenom}");
+        Console.WriteLine($"téléphone:{client.Tel}");
         Console.WriteLine($"nom:{client.Nom}");
         Console.WriteLine($"CIN:{client.CIN}");
     }
-
-    //méthode Crediter permettant de Crediter le compte, prenant une somme et un compte en paramètres.
-
 }
-}
+//méthode Crediter permettant de Crediter le compte, prenant une somme et un compte en paramètres.
+//méthode Crediter permettant de Crediter le compte, prenant une somme et un compte en paramètres.
+//méthode Crediter permettant de Crediter le compte, prenant une somme et un compte en paramètres.
+
+
+
+

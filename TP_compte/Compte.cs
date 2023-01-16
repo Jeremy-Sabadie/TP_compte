a076c7c4a6cd044c3fe7d82640a1dc7cf044e0d7
cda07saba <jeremy.sabadie@2isa.org>
2023-01-16 09:39:41 +0100

    Ajout de la classe compte -Création d'un constructeur de compte.

---
+++ TP_compte/Compte.cs
@@ -0,0 +1,23 @@
+﻿namespace TP_compte
+{
+    internal class Compte
+    {//Attributs de la classe compte:
+        private double Sold;
+        private int Code;
+        private Client owner;
+        //Constructeur par défault qui incrémente le code du compte lors de sa création:
+        public Compte()
+        {
+            Code++;
+        }
+
+        //Attribut solde en lecture seule.
+        public double Sold1 { get => Sold; private set => Sold = value; }
+        //Attribut code du compte en lecture seule.
+        public int Code1 { get => Code; private set => Code = value; }
+        internal Client Owner { get => owner; set => owner = value; }
+    }
+    //Constructeur par défault:
+
+}
+}
